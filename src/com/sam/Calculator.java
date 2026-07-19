4ff51b328bcdd8124504f3e017390a718c02052a
Samhitha-68 <samhitha1120@gmail.com>
2026-07-19 09:50:03 +0530

    Java concepts with solved problems

--- src/com/sam/Calculator.java
+++ src/com/sam/Calculator.java
@@ -1,5 +1,42 @@
 package com.sam;
+import java.util.*;
 
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //taking input from user until they press X or x
+        int ans=0;
+        while(true){
+            System.out.println("Enter the operation : ");
+            char op=sc.next().trim().charAt(0);
+            if(op=='+' || op=='-' || op=='*' || op=='/' || op=='%') {
+                System.out.println("Enter two numbers : ");
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
 
+                if(op=='+'){
+                    ans=num1+num2;
+                }
+                else if(op=='-'){
+                    ans=num1-num2;
+                }
+                else if(op=='*'){
+                    ans=num1*num2;
+                }
+                else if(op=='/'){
+                    if(num2 != 0){
+                        ans=num1/num2;
+                    }
+                }
+                else if(op=='%') {
+                    ans = num1 % num2;
+                }
+            }else if(op == 'X' || op == 'x') {
+                break;
+            }else {
+                System.out.println("Invalid operation");
+            }
+            System.out.println(ans);
+        }
+    }
 }
---
+++ src/com/sam/EnhancedSwitch2.java
@@ -0,0 +1,32 @@
+package com.sam;
+import java.util.*;
+
+public class EnhancedSwitch2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+            case 6, 7 -> System.out.println("Weekend");
+            default -> System.out.println("Enter valid day");
+        }
+    }
+}
+
+//equivalent switch case
+//switch(day){
+//        case 1:
+//        case 2:
+//        case 3:
+//        case 4:
+//        case 5:
+//        System.out.println("Weekday");
+//                break;
+//                        case 6:
+//                        case 7:
+//                        System.out.println("Weekend");
+//                break;
+//default :
+//        System.out.println("Enter valid day");
+//        }
